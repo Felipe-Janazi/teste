b0d12c55f79fd6632e52e7187163dc106a526720
Felipe <felipe.janazi@sptech.school>
2024-11-04 20:27:56 -0300

    Script

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,1 +1,52 @@
-COLOCAR TODOS SCRIPTS
+CREATE DATABASE vortex2;
+USE vortex2;
+
+
+CREATE TABLE Empresa (
+    idEmpresa INT PRIMARY KEY,
+    nome VARCHAR(45),
+    cnpj VARCHAR(45),
+    cep VARCHAR(45),
+    cidade VARCHAR(45),
+    bairro VARCHAR(45),
+    numero VARCHAR(45)
+);
+
+CREATE TABLE Usuarios (
+    idUsuarios INT PRIMARY KEY,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45)
+);
+
+CREATE TABLE Funcionarios (
+    idFuncionarios INT PRIMARY KEY,
+    cpf VARCHAR(45),
+    cargo VARCHAR(45),
+    fkUsuario INT,
+    fkGerente INT,
+    fkEmpresa INT,
+    FOREIGN KEY (fkUsuario) REFERENCES Usuarios(idUsuarios),
+    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+);
+
+CREATE TABLE DadosInseridos (
+    idDadosInseridos INT PRIMARY KEY,
+    bairro VARCHAR(45),
+    cidade VARCHAR(45),
+    valorM2 VARCHAR(45),
+    densidade VARCHAR(45),
+    dtInsercao VARCHAR(45),
+    idh  INT
+);
+
+CREATE TABLE Simulacao (
+    idSimulacao INT PRIMARY KEY,
+    dtSimulacao VARCHAR(45),
+    zona VARCHAR(45),
+    bairro VARCHAR(45),
+    fkFuncionario INT,
+    fkDadosInseridos INT,
+    FOREIGN KEY (fkFuncionario) REFERENCES Funcionarios(idFuncionarios),
+    FOREIGN KEY (fkDadosInseridos) REFERENCES DadosInseridos(idDadosInseridos)
+);
